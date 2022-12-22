532f5b00563344e979e4c2c0cd9a88a6edaa50e9
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-12-22 23:16:20 +0200

    need fix errors

---
+++ controlers/contacts/removeById.js
@@ -0,0 +1,17 @@
+const { Contact } = require("../../models");
+
+const removeById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+
+  if (!result) {
+    throw new Error(`Contact with id ${contactId} not found`);
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: { result },
+  });
+};
+
+module.exports = removeById;
