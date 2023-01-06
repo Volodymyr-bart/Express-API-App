550b7c34539d1bdc1a3fdd178d8065c37b229278
Volodymyr Bortokhov <bartzh1991@gmail.com>
2023-01-06 12:49:10 +0200

    edit current

--- controlers/users/getCurrent.js
+++ controlers/users/getCurrent.js
@@ -1,13 +1,13 @@
 // const { User } = require("../../models/");
 const getCurrent = async (req, res) => {
-  const { name, email } = req.user;
+  const { subscription, email } = req.user;
   res.json({
     status: "success",
     code: 200,
     data: {
       user: {
-        name,
         email,
+        subscription,
       },
     },
   });
