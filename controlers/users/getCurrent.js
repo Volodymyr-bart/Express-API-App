85da3ee7c6ca7b855c8c5d5579c41a88e2707b66
Volodymyr Bortokhov <bartzh1991@gmail.com>
2023-01-06 15:41:43 +0200

    fix

--- controlers/users/getCurrent.js
+++ controlers/users/getCurrent.js
@@ -1,15 +1,19 @@
 // const { User } = require("../../models/");
 const getCurrent = async (req, res) => {
   const { subscription, email } = req.user;
+  // res.json({
+  //   status: "success",
+  //   code: 200,
+  //   data: {
+  //     user: {
+  //       email,
+  //       subscription,
+  //     },
+  //   },
+  // });
   res.json({
-    status: "success",
-    code: 200,
-    data: {
-      user: {
-        email,
-        subscription,
-      },
-    },
+    email,
+    subscription,
   });
 };
 module.exports = getCurrent;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,13 +8,13 @@ const { joiSchema, favoriteJoiSchema } = require("../../models/contact");
 
 router.get("/", auth, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", auth, ctrlWrapper(ctrl.getById));
 
 router.post("/", auth, validation(joiSchema), ctrlWrapper(ctrl.add));
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+router.delete("/:contactId", auth, ctrlWrapper(ctrl.removeById));
 
-router.put("/:contactId", validation(joiSchema), ctrlWrapper(ctrl.updateById));
+router.put("/:contactId", auth, validation(joiSchema), ctrlWrapper(ctrl.updateById));
 
 router.patch(
   "/:contactId/favorite",
