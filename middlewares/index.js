a80c10510efe71bd34e8641a605e820e3544a73c
Volodymyr Bortokhov <bartzh1991@gmail.com>
2023-01-11 20:08:56 +0200

    preparing multer

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const ctrlWrapper = require("./ctrlWrapper");
 const validation = require("./validation");
 const HttpError = require("./HttpError");
 const auth = require("./auth");
+const upload = require("./upload");
 
 module.exports = {
   ctrlWrapper,
   validation,
   HttpError,
   auth,
+  upload,
 };
